5fc9bc9eddaffa770580019f8453dfc008760822
戴亮金 <dailiangjin@vv.cn>
2021-11-08 15:11:02 +0800

    Swift 基本结构迁移完成，编译成功

--- YunZaiApp/AppDelegate.swift
+++ YunZaiApp/AppDelegate.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
     var window: UIWindow?
     var audioPlayer: AVAudioPlayer?
     var localCallNotification: UILocalNotification?
@@ -24,12 +24,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         NotificationCenter.default.addObserver(self, selector: #selector(onFriendRequestUpdated(notification:)), name: NSNotification.Name(rawValue: kFriendRequestUpdated), object: nil)
         
 #if WFCU_SUPPORT_VOIP
-    //音视频高级版不需要stun/turn服务，请注释掉下面这行。单人版和多人版需要turn服务，请自己部署然后修改配置文件。
+        //音视频高级版不需要stun/turn服务，请注释掉下面这行。单人版和多人版需要turn服务，请自己部署然后修改配置文件。
         WFAVEngineKit.shared().addIceServer(ICE_ADDRESS, userName: ICE_USERNAME, password: ICE_PASSWORD)
         WFAVEngineKit.shared().setVideoProfile(kWFAVVideoProfile360P, swapWidthHeight: true)
         WFAVEngineKit.shared().delegate = self
 #endif
-    
+        
         WFCUConfigManager.global().appServiceProvider = AppService.shared()
         WFCUConfigManager.global().fileTransferId = FILE_TRANSFER_ID
         
@@ -42,12 +42,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let savedUserId = UserDefaults.standard.string(forKey: "savedUserId")
         
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = ViewController();
+        window?.rootViewController = WFCBaseTabBarController();
         window?.backgroundColor = .white
         window?.makeKeyAndVisible()
         
         setupNavBar()
         //setQrCodeDelegate(self);
+        
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            if error == nil {
+                print("succeeded!")
+                DispatchQueue.main.async {
+                    application.registerForRemoteNotifications()
+                }
+            }
+        }
+        
+        if savedToken?.count ?? 0 > 0 && savedUserId?.count ?? 0 > 0 {
+            //需要注意token跟clientId是强依赖的，一定要调用getClientId获取到clientId，然后用这个clientId获取token，这样connect才能成功，如果随便使用一个clientId获取到的token将无法链接成功。另外不能多次connect，如果需要切换用户请先disconnect，然后3秒钟之后再connect（如果是用户手动登录可以不用等，因为用户操作很难3秒完成，如果程序自动切换请等3秒）。
+            WFCCNetworkService.sharedInstance().connect(savedUserId, token: savedToken)
+        } else {
+            let loginVC = WFCLoginViewController()
+            let nav = UINavigationController.init(rootViewController: loginVC)
+            self.window?.rootViewController = nav
+        }
+        
         
         return true
     }
@@ -57,15 +78,17 @@ extension AppDelegate {
     @objc func onFriendRequestUpdated(notification: Notification) {
         if UIApplication.shared.applicationState == .background {
             let newRequests = notification.object as? [String]
-//            if newRequests?.count {
-//                return
-//            }
+            //            if newRequests?.count {
+            //                return
+            //            }
         }
+    }
+    
+    func setupNavBar() {
+        WFCUConfigManager.global().setupNavBar()
     }
 }
 
 extension AppDelegate {
-    func setupNavBar() {
-        WFCUConfigManager.global().setupNavBar()
-    }
+    
 }
